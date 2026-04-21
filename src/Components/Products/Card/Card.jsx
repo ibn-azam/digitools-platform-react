64241500453496369fddb0c3b40eb181ba4764d6
ibn-azam <farhan.tsx@gmail.com>
2026-04-21 20:50:08 +0100

    fix some ui problems

--- src/Components/Products/Card/Card.jsx
+++ src/Components/Products/Card/Card.jsx
@@ -18,8 +18,8 @@ const Card = ({cardData ,handleSelectedProducts}) => {
     return (
         <div className='container mx-auto'>
             <div className='px-6 py-3 min-w-95 min-h-80 bg-[#ffffff] border-2 border-[#f2f2f2] rounded-2xl shadow-md hover:translate-y-1'>
-                <div className="flex justify-between ">
-                    <span><img src={cardData.icon} alt="" /></span>
+                <div className="flex items-center justify-between ">
+                    <span className="border-2 border-[#f2f2f2] p-2.5 rounded-full"><img className="w-8 h-8" src={cardData.icon} alt="" /></span>
                     <span className={cardData.tag === "Best Seller" ? `bg-[#fef3c6] text-[#bb4d00] px-2
                         py-1 rounded-full `:cardData.tag === "Popular" ? `bg-[#e1e7ff] text-[#4f39f6] px-2
                         py-1 rounded-full` : `bg-[#dbfce7] text-[#0a883e] px-2
@@ -30,7 +30,7 @@ const Card = ({cardData ,handleSelectedProducts}) => {
                     <p className="text-[16px] text-[#627382] mb-2">{cardData.description}</p>
                     <h2><span className="text-[24px] font-bold text-[#101727]">${cardData.price}</span><span className="text-[#627382] text-[16px]">/{cardData.period}</span></h2>
                      {
-                       cardFeatures.map(feature =><Features feature={feature}></Features>) 
+                       cardFeatures.map((feature,index) =><Features feature={feature} key={index}></Features>) 
                     }
                 </div>
                 <button onClick={handleBuynowBtn} className={`btn ${isSelected === true ? "w-full my-2 rounded-full text-white font-bold bg-green-600" : "w-full my-2 rounded-full text-white font-bold bg-linear-to-r from-[#4f39f6] to-[#9514fa]"}`}>Buy Now</button>
